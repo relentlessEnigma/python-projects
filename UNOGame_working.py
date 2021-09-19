f775fd5fbd7609d685d902867955de720e836f9d
Linger91 <61918822+linger91@users.noreply.github.com>
2021-09-19 20:39:17 +0100

    Update UNOGame_working.py

--- UNOGame_working.py
+++ UNOGame_working.py
@@ -1,3 +1,5 @@
+#This is a python file
+
 #UNO Game from beginning
 
 
@@ -12,10 +14,14 @@ import time  # use only for debugging
 # ----------- Global Lists: - Approved []
 Deck_Specials = ["Draw Two", "Reverse", "Skip", "Wild", "Wild Draw Four"]  # Implement maybe later...
 Deck_Colors = ["|Red", "|Blu", "|Yel", "|Gre"]
+ActionCard = ["|Gre Skip|", "|Blu Skip|", "|Red Skip|", "|Yel Skip|", "|Gre Reverse|", "|Blu Reverse|", "|Red Reverse|", "|Yel Reverse|", "|Gre Draw Two|", "|Blu Draw Two|", "|Red Draw Two|", "|Yel Draw Two|"]
+WildCards = ["|Wild Draw Four|", "|Wild|"]
 Deck_CurrentGame = []
-Deck_TableDeck = []
+Deck_CurrentGame_Obj = []
+Deck_TableDeck_Obj = []
 Players_List = []
 playingOrder = [1]
+
 # ----------- Create Deck: - Approved []
 def createDeck():
     def createNumbersOnDeck(RangeMin, RangeMax, Ammount):  # Create Deck with numbers
@@ -40,6 +46,82 @@ def createDeck():
         shuffle(Deck_CurrentGame)  # Shuffle the deck
     compileDeck()
 
+class Card():
+    def __init__(self, name, color, number, isAction, actionType, isWild, wildType):
+        # Card attributes
+        self.name = name
+        self.color = color
+        self.number = number
+        self.isAction = isAction
+        self.actionType = actionType
+        self.isWild = isWild
+        self.wildType = wildType
+
+#Define card attributes:
+def cardAttrs(card, call_isCardWild=0, call_cardWildType=0, call_isCardAction=0, call_cardActionType=0, call_getCardColor=0, call_getCardNumber=0):
+    def isCardWild(card):
+        if card in WildCards:
+            return True
+        else:
+            return False
+    def cardWildType(card):
+        if isCardWild(card) == True:
+            if card == "|Wild|":
+                type = "Wild"
+                return type
+            elif card == "|Wild Draw Four|":
+                type = "Wild Draw Four"
+                return type
+
+    def isCardAction(card):
+        if card in ActionCard:
+            return True
+        else:
+            return False
+    def cardActionType(card):
+        if isCardAction(card) == True:
+            type = "".join(char for char in card[5:-1:1])
+            return type
+
+    def getCardColor(card):
+        if isCardWild(card) == True:
+            color = None
+            return color
+        else:
+            color = "".join(char for char in card[1:4:1])
+            return color
+    def getCardNumber(card):
+        if isCardWild(card) or isCardAction(card):
+            number = None
+            return number
+        else:
+            number = "".join(char for char in card[5:6:1])
+            return number
+
+    if call_isCardWild == 1:
+        return isCardWild(card)
+    elif call_cardWildType == 1:
+        return cardWildType(card)
+    elif call_isCardAction == 1:
+        return isCardAction(card)
+    elif call_cardActionType == 1:
+        return cardActionType(card)
+    elif call_getCardColor == 1:
+        return getCardColor(card)
+    elif call_getCardNumber == 1:
+        return getCardNumber(card)
+
+#Convert cards to object and append to new List
+def convertCardToObject():
+    for card in Deck_CurrentGame: #Append each card as an object to a new deck
+        Deck_CurrentGame_Obj.append(Card(card,
+                                     cardAttrs(card, 0, 0, 0, 0, 1, 0),
+                                     cardAttrs(card, 0, 0, 0, 0, 0, 1),
+                                     cardAttrs(card, 0, 0, 1, 0, 0, 0),
+                                     cardAttrs(card, 0, 0, 0, 1, 0, 0),
+                                     cardAttrs(card, 1, 0, 0, 0, 0, 0),
+                                     cardAttrs(card, 0, 1, 0, 0, 0, 0)))
+
 # ----------- Class Player (Create Player): - Approved []
 class Player:  # Define Players and Player Hand
     def __init__(self, playerID, playerName, playerAge, playerScore, mainDeck):
@@ -53,11 +135,11 @@ class Player:  # Define Players and Player Hand
         for i in range(Ammount):
             self.playerHand.append(self.mainDeck.pop(0))
     def showPlayerHand(self):
-        count = 1
+        count = 0
         time.sleep(1)
         print(self.playerName, "'s Hand:")
         for i in self.playerHand:
-            print(count, " - ", i)
+            print(count, " - ", i.name)
             count += 1
     def setPlayerID(self, playerID):
         self.playerID = playerID
@@ -82,73 +164,56 @@ def lowestAge():
             z = Players_List[player]
     print(f"{z.playerName}, starts the game because has the lowest age.")
     return z
+
 # --------- Moves
 def playerHandToTable(move, Player):
-    Deck_TableDeck.insert(0, Player.playerHand[move])
+    Deck_TableDeck_Obj.insert(0, Player.playerHand[move])
     Player.playerHand.pop(move)
-    print(Player.playerName, " played the card: ", Deck_TableDeck[0])
+    print(f"{Player.playerName} played the card: {Deck_TableDeck_Obj[0].name}")
 
 def deckToTable():
-    Deck_TableDeck.insert(0, Deck_CurrentGame[-1])
-    Deck_CurrentGame.pop(-1)
+    Deck_TableDeck_Obj.insert(0, Deck_CurrentGame_Obj[-1])
+    Deck_CurrentGame_Obj.pop(-1)
 
 def seeCardOnTable():
-    print("\nCard on table is:\n", Deck_TableDeck[0], "\n")
+    print(f"\nCard on table is:\n{Deck_TableDeck_Obj[0].name}\n")
+
 
 #---------- Checks cards eligible play:
 def checkActionCardsStartOfRound(Player):
-    if Deck_TableDeck[0] == "Wild Draw Four":
-        print(f"{Player.playerName} picked up 4 cards from the main deck. Cant play this turn, sorry!")
-        Player.setPlayerHand(4)
-        return True
-    elif Deck_TableDeck[0] == "Draw Two":
+    if Deck_TableDeck_Obj[0].name == "Draw Two":
         print(Player.playerName, " pick up 2 cards from the main deck! :p")
         Player.setPlayerHand(2)
         return True
+def checkActionCardsEndOfRound(Player, turn):
+    if Deck_TableDeck_Obj[0].isAction == True:
+        if Deck_TableDeck_Obj[0].actionType == "Skip":
+            print("Will skip the next Player!")
+            playerTurn += 1
+        elif Deck_TableDeck_Obj[0].actionType == "Reverse":
+            print("Will Reverse the Playing Order.")
+            playingOrder.append[-1]
+        return True
 
-def checkActionCardsEndOfRound(): #Check first special cards
-    if Deck_TableDeck[0] == "Reverse":
-        if playingOrder[-1] == -1:
-            playingOrder.append(1)
+def checkCardPlay(Player, move):
+    if Player.playerHand[move].color == Deck_TableDeck_Obj[0].color:
+        playerHandToTable(move, Player)
+    elif Player.playerHand[move].number == Deck_TableDeck_Obj[0].number:
+        playerHandToTable(move, Player)
+    elif Player.playerHand[move].isWild:
+        playerHandToTable(move, Player)
+    else:
+        print("You don't have cards to play, go draw 1.")
+        Player.setPlayerHand(1)
+        move = Player.playerHand[-1]
+        if Player.playerHand[move].color == Deck_TableDeck_Obj[0].color:
+            playerHandToTable(move, Player)
+        elif Player.playerHand[move].number == Deck_TableDeck_Obj[0].number:
+            playerHandToTable(move, Player)
+        elif Player.playerHand[move].isWild:
+            playerHandToTable(move, Player)
         else:
-            playingOrder.append(-1)
-    elif Deck_TableDeck[0] == "Skip":
-        if playingOrder[-1] == -1:
-            playingOrder[-1] == -2
-        else:
-            playingOrder[-1] == 2
-
-def has_numbers(inputString):
-    return any(char.isdigit() for char in inputString)
-
-def checkCardNumber(Player):
-    for tableCard in Deck_TableDeck[0]:
-        if has_numbers(tableCard) == True:
-            cardNumberOnTable = tableCard
-            for playerCard in Player.playerHand:
-                if has_numbers(playerCard) == True:
-                    cardNumberOnPlayerHand = playerCard
-                    if cardNumberOnPlayerHand == cardNumberOnTable:
-                        return True
-
-def checkCardColor(Player):
-    for color in Deck_Colors:
-        if color in Deck_TableDeck[0]:
-            tableCardColor = color
-    for playerCard in Player.playerHand:
-        for color in Deck_Colors:
-            if color in playerCard:
-                if color == tableCardColor:
-                    return True
-
-def playerMove():
-    playerTurn.showPlayerHand()
-    move = input("Select the card you want to play: ")
-    if checkCardNumber() == True:
-        #Falta check para confirmar a carta q o jogador quer jogar vs a que esta na mesa.
-
-    return move
-
+            print("Not lucky ... Next Player.")
 
 #1 -create deck - ok
 #2 - create player - ok
@@ -165,20 +230,24 @@ def playerMove():
 #12 - in the end of each round, calculate the index with the playingOrder[-1] to calculate the index of the next player
 
 createDeck() #1
-createPlayer(Deck_CurrentGame, Players_List) #2
+convertCardToObject()
+createPlayer(Deck_CurrentGame_Obj, Players_List) #2
 for player in Players_List: player.setPlayerHand(7) #3
 deckToTable() #4
-lowestAge() #5
 
-startingPlayer = 0
-playerTurn = Players_List[startingPlayer]
+startingPlayer = lowestAge()
+playerTurn = Players_List[startingPlayer.playerID]
+seeCardOnTable() #7
+print(f"Player Turn: {playerTurn.playerName}")
+playerTurn.showPlayerHand()
 
-while(True):
-    seeCardOnTable()
-    print(f"Player Turn: {playerTurn.playerName}")
-    if checkActionCardsStartOfRound(playerTurn) == True:
-        playerTurn += playingOrder[-1]
-        playerMove()
-        continue
-    playerMove()
-    checkActionCardsEndOfRound()
+playerMove = int(input("Select the card to play: "))
+checkCardPlay(playerTurn, playerMove)
+
+nextPlayer = playerTurn.playerID + playingOrder[-1]
+Player = Players_List[nextPlayer]
+while (True):
+    print(f"Player Turn: {Player.playerName}")
+    nextPlayer.showPlayerHand()
+    playerMove = int(input("Select the card to play: "))
+    checkCardPlay(nextPlayer, playerMove)
